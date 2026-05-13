5b45480415fe3248d924465749dd2566fc53794d
Taqi Ahmed <taqi26ahmed@gmail.com>
2026-05-14 02:12:32 +0300

    Created Types Files

--- src/Types.h
+++ src/Types.h
@@ -3,6 +3,9 @@
 struct Position {
     int x;
     int y;
+
+    bool operator==(const Position& other) const;
+    bool operator<(const Position& other) const;
 };
 
 struct Wall {
@@ -15,6 +18,8 @@ struct Player {
     bool won_game;
     int walls_left; // Starts at 10 for a 2-player game
     int goal_row;   // P1's goal is row 8 (or 9 in 1-index), P2's is row 0 (or 1)
+
+    
 };
 
 #endif // TYPES_H
