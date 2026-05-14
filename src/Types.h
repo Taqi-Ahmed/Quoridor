62f06db45cf5267e97e3eaaf3902bd7d1ecf1af7
farahzayed22 <farahzayed418@gmial.com>
2026-05-14 04:22:15 -0700

    valid wall implementation

--- src/Types.h
+++ src/Types.h
@@ -1,25 +1,26 @@
 #ifndef TYPES_H
 #define TYPES_H
-struct Position {
+struct Position
+{
     int x;
     int y;
 
-    bool operator==(const Position& other) const;
-    bool operator<(const Position& other) const;
+    bool operator==(const Position &other) const;
+    bool operator<(const Position &other) const;
 };
 
-struct Wall {
+struct Wall
+{
     Position pos;
     bool isHorizontal;
 };
 
-struct Player {
+struct Player
+{
     Position pos;
     bool won_game;
     int walls_left; // Starts at 10 for a 2-player game
     int goal_row;   // P1's goal is row 8 (or 9 in 1-index), P2's is row 0 (or 1)
-
-    
 };
 
 #endif // TYPES_H
--- src/game.cpp
+++ src/game.cpp
@@ -1,1 +1,39 @@
 #include "game.h"
+bool GameBoard::is_valid_wall_placement(bool is_horizontal, int r, int c){
+    bool valid=false;
+    if (r>8 || r<1 || c>8 || c<1) return false;
+        //check for any possible collisions may occur before placing the wall at this position
+        // wall is horizontal then y is const ( row gives me y value , colunmn gives me x value)
+        // wall is  vertival then x is const
+        for (auto& wall: walls){
+
+            if(wall.isHorizontal && is_horizontal ){
+               if((wall.pos.y==r) && (wall.pos.x==c || wall.pos.x==c-1 || wall.pos.x==c+1))
+                return false; }
+
+            if (! wall.isHorizontal && ! is_horizontal) {
+               if((wall.pos.x==c) && (wall.pos.y==r || wall.pos.y==r-1 || wall.pos.y==r+1))
+                return false; }
+
+            if (is_horizontal && ! wall.isHorizontal){
+                if(wall.pos.y==r && wall.pos.x== c) return false;
+            }
+
+            if (!is_horizontal && wall.isHorizontal){
+                if(r== wall.pos.y && c==wall.pos.x) return false;
+            }
+
+        }
+
+        //check when the added wall blocks the opponent move or not
+        Wall tempWall={{c,r}, is_horizontal};
+        walls.push_back(tempWall);
+        valid =BFS_search();
+        walls.pop_back();
+
+        return valid;
+
+    }
+
+
+
